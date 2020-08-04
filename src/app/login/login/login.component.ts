1a8a6f17dbef9cb510f87a34d4572e4dc3678498
Joseph Hissong <hissong.joseph@gmail.com>
2020-08-04 19:46:25 -0400

    fix login issue

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -23,7 +23,10 @@ export class LoginComponent implements OnInit {
   }
 
   loginWithGmail() {
-    this.fireAuth.signInWithPopup(new auth.GoogleAuthProvider());
-    this.router.navigate[('')];
+    this.fireAuth.signInWithPopup(new auth.GoogleAuthProvider()).then(
+      res => {
+        this.router.navigate(['']);
+      }
+    );
   }
 }
